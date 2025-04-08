d4ffbf8aa70d1eda2e2c83687d659441d43fd545
João Zolet <zolet360@gmail.com>
2025-04-08 14:15:18 -0300

    firebase add

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { firebaseConfig } from "./firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { storage };
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxHs3LvuUHE9wjODeM68J7CBjvhT8pKrA",
+  authDomain: "sat-agro-7d375.firebaseapp.com",
+  projectId: "sat-agro-7d375",
+  storageBucket: "sat-agro-7d375.firebasestorage.app",
+  messagingSenderId: "648496362200",
+  appId: "1:648496362200:web:8529d00dc6482e1b1a9dad",
+  measurementId: "G-727DN5EB66",
+};
